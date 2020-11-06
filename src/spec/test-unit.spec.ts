21ca22e7c575c31a52fc1022939764afca3cf4ef
kaeross <kaeross@icloud.com>
2020-11-06 13:51:09 +0000

    add further tests

--- src/spec/test-unit.spec.ts
+++ src/spec/test-unit.spec.ts
@@ -7,6 +7,14 @@ describe('Submit entry', () => {
     const gameHandler = new GameHandler();
 
     it('should correctly score the palendrome phrase "Al lets Della call Ed \'Stella.\'"', () => {
-        expect(gameHandler.submitEntry({ name: 'test', word: "Al lets Della call Ed \'Stella.\'" }) === 31).toBe(true);
+        expect(gameHandler.submitEntry({ name: 'test', word: "Al lets Della call Ed \'Stella.\'" })).toBe(31);
+    });
+
+    it('should find that 9oo9 is a palendrome', () => {
+        expect(gameHandler.submitEntry({ name: 'test', word: '9oo9' })).toBeGreaterThan(0);
+    });
+
+    it('should score the word paledrome 0', () => {
+        expect(gameHandler.submitEntry({ name: 'test', word: 'palendrome' })).toBe(0);
     });
 });
